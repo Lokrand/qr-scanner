d594fd199de641515ab43be432f09804115d2eb5
Dima <Uchiha132132qqq@gmail.com>
2023-01-31 19:27:40 +0300

    добавил побольше тестов

--- src/utils/validateUrl.test.js
+++ src/utils/validateUrl.test.js
@@ -2,4 +2,11 @@ import { validateUrl } from "./validateUrl";
 
 test("Валидация ссылки", () => {
   expect(validateUrl("http://localhost:3000")).toBe(true);
+  expect(validateUrl("ya.ru")).toBe(true);
+  expect(validateUrl("google.com")).toBe(true);
+  expect(validateUrl("some strings")).toBe(false);
+  expect(validateUrl("412123123")).toBe(false);
+  expect(validateUrl("https://www.npmjs.com/package/react-qr-code")).toBe(true);
+  expect(validateUrl("мвд.рф")).toBe(true);
+  expect(validateUrl("https://web.telegram.org/z/#113947037")).toBe(true);
 });
