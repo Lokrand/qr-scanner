eb46877f44d4f8a31622374bf2301d0ede2f432c
Dima <Uchiha132132qqq@gmail.com>
2023-02-06 23:03:38 +0300

    formatting

--- src/components/AccuracyBlock/AccuracyBlock.tsx
+++ src/components/AccuracyBlock/AccuracyBlock.tsx
@@ -1,7 +1,7 @@
-import React, { useState, FC, Dispatch, SetStateAction } from 'react';
+import React, { useState, FC, Dispatch, SetStateAction } from "react";
 import styles from "./AccuracyBlock.module.css";
-import { Arrow } from '../../icons/Arrow/Arrow';
-import { Accuracy } from './Accuracy/Accuracy';
+import { Arrow } from "../../icons/Arrow/Arrow";
+import { Accuracy } from "./Accuracy/Accuracy";
 
 const accuracy = ["L", "M", "Q", "H"];
 
@@ -10,38 +10,41 @@ interface IAccuracyBlock {
   setQrAccuracy: Dispatch<SetStateAction<string>>;
 }
 
-export const AccuracyBlock: FC<IAccuracyBlock> = ({ qrAccuracy, setQrAccuracy }) => {
+export const AccuracyBlock: FC<IAccuracyBlock> = ({
+  qrAccuracy,
+  setQrAccuracy,
+}) => {
   const [popupActive, setPopupActive] = useState(false);
   return (
     <div className={styles.accuracyBlock}>
-        <p>Accuracy of the qr code -</p>
-        <div>{qrAccuracy}</div>
-        <div
-          className={
-            popupActive
-              ? `${styles.accuracyBlock__arrow} ${styles.accuracyBlock__arrow_active}`
-              : styles.accuracyBlock__arrow
-          }
-          onClick={() => setPopupActive(!popupActive)}
-        >
-          <Arrow />
-        </div>
-        {popupActive && (
-          <div className={styles.accuracyBlock__list}>
-            {accuracy.map((el, index) => {
-              return (
-                <Accuracy
-                  key={index}
-                  quality={el}
-                  onClick={() => {
-                    setQrAccuracy(el);
-                    setPopupActive(false);
-                  }}
-                />
-              );
-            })}
-          </div>
-        )}
+      <p>Accuracy of the qr code -</p>
+      <div>{qrAccuracy}</div>
+      <div
+        className={
+          popupActive
+            ? `${styles.accuracyBlock__arrow} ${styles.accuracyBlock__arrow_active}`
+            : styles.accuracyBlock__arrow
+        }
+        onClick={() => setPopupActive(!popupActive)}
+      >
+        <Arrow />
       </div>
-  )
-}
+      {popupActive && (
+        <div className={styles.accuracyBlock__list}>
+          {accuracy.map((el, index) => {
+            return (
+              <Accuracy
+                key={index}
+                quality={el}
+                onClick={() => {
+                  setQrAccuracy(el);
+                  setPopupActive(false);
+                }}
+              />
+            );
+          })}
+        </div>
+      )}
+    </div>
+  );
+};
